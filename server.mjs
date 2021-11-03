5d70e5a35a0fc8fc4efb39d52b3056554b1351e0
Noman Qureshi <83730944+studyndev@users.noreply.github.com>
2021-11-03 18:41:52 +0500

    adding routes
    from SMiT PC

--- server.mjs
+++ server.mjs
@@ -7,6 +7,14 @@ app.get('/',(req,res)=>{
   res.send('WELCOME To Deploy!');
 })
 
+app.get('/about',(req,res)=>{
+  res.send('This is about Page...');
+})
+
+app.get('/profile',(req,res)=>{
+  res.send('This is profile page...');
+})
+
 app.listen(PORT,()=>{
   console.log(`APP is Running on ${PORT}`);
 })
